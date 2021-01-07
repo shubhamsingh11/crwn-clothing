1be41bece0fd0b3121b432fdd432b7a1f3a961cc
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-01-07 23:50:49 +0530

    Added Collection Page with nested routing

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component'
+
+import './collection.style.scss';
+
+const CollectionPage = ({ match }) => (
+    <div className='category'>
+        <h2>CATEGORY PAGE</h2>
+    </div>
+
+)
+
+export default CollectionPage;
