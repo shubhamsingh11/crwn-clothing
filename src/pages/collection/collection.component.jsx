309cd4dc1a812162779fa94c668a885ae54e484e
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-01-08 23:46:26 +0530

    updated shop selector to map id from the collectionURLParam

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import CollectionItem from '../../components/collection-item/collection-item.component'
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.style.scss';
 
@@ -11,4 +14,8 @@ const CollectionPage = ({ match }) => (
 
 )
 
-export default CollectionPage;
+const mapsStatesToProps = (state , ownProps) => ({
+    collection : selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapsStatesToProps)(CollectionPage);
